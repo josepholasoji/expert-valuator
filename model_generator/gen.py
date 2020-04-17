c4a13a02925d1b5f09b06fbf09e00dce73ef3816
olasoji.joseph <olasoji.joseph@interswitchgroup.com>
2020-04-17 01:15:36 +0100

    add a tensorflow random forest training code the implementation

--- model_generator/gen.py
+++ model_generator/gen.py
@@ -7,79 +7,80 @@ from tensorflow.keras.preprocessing import text as txtProcessor
 from tensorflow.keras.preprocessing.text import Tokenizer
 
 
+def generate():
+
+    #Parse and tokenized the library data
+    tenkenizer = Tokenizer(num_words=None, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split='\,', char_level=False, oov_token=None)
+    tenkenizer.fit_on_texts(utils.libraries.split(","))
+    librarySequence = tenkenizer.word_index
+
+    #Parse and tokenized the language data
+    tenkenizer = Tokenizer(num_words=None, filters='!"$%&()*,./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split='\,', char_level=False, oov_token=None)
+    tenkenizer.fit_on_texts(utils.programming_languages.split(","))
+    programming_laguage_sequence = tenkenizer.word_index
+
+    #Parse and tokenized the classification data
+    tenkenizer = Tokenizer(num_words=None, filters='!"$%&()*,./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split='\,', char_level=False, oov_token=None)
+    tenkenizer.fit_on_texts(utils.classifications.split(","))
+    classifications_sequence = tenkenizer.word_index
+
+    #load the template data
+    template = (json.loads(utils.template_data))
+
+    language_of_interest = []
+    for language in template['languages']:
+        language = language.lower()
+        if language in programming_laguage_sequence:
+            token = programming_laguage_sequence[language] 
+        else:
+            token = programming_laguage_sequence["none"]    
+
+        language_of_interest.append(token)
+
+    lib_of_interest = []
+    for lib in template['technology']:
+        lib = lib.lower()
+        if lib in librarySequence:
+            token = librarySequence[lib] 
+        else:
+            token = librarySequence["none"]    
+            
+        lib_of_interest.append(token)
 
 
-#Parse and tokenized the library data
-tenkenizer = Tokenizer(num_words=None, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split='\,', char_level=False, oov_token=None)
-tenkenizer.fit_on_texts(utils.libraries.split(","))
-librarySequence = tenkenizer.word_index
+    header = ["language",
+                "library",
+                "imported_library",
+                "commit_count",
+                "days_since_last_commit",
+                "code_churn",
+                "classificatioin"]
 
-#Parse and tokenized the language data
-tenkenizer = Tokenizer(num_words=None, filters='!"$%&()*,./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split='\,', char_level=False, oov_token=None)
-tenkenizer.fit_on_texts(utils.programming_languages.split(","))
-programming_laguage_sequence = tenkenizer.word_index
+    table = []
+    table.append(header)
 
-#Parse and tokenized the classification data
-tenkenizer = Tokenizer(num_words=None, filters='!"$%&()*,./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split='\,', char_level=False, oov_token=None)
-tenkenizer.fit_on_texts(utils.classifications.split(","))
-classifications_sequence = tenkenizer.word_index
+    #for disqualified
+    for entry_data in template['disqualified']:
+        entry = utils.gen_classification_data(entry_data, librarySequence, programming_laguage_sequence, classifications_sequence)
+        table+=(entry)
 
-#load the template data
-template = (json.loads(utils.template_data))
+    #for expert
+    for entry_data in template['expert']:
+        entry = utils.gen_classification_data(entry_data, librarySequence, programming_laguage_sequence, classifications_sequence)
+        table+=(entry)
 
-language_of_interest = []
-for language in template['languages']:
-    language = language.lower()
-    if language in programming_laguage_sequence:
-        token = programming_laguage_sequence[language] 
-    else:
-        token = programming_laguage_sequence["none"]    
+    #for novice
+    for entry_data in template['novice']:
+        entry = utils.gen_classification_data(entry_data, librarySequence, programming_laguage_sequence, classifications_sequence)
+        table+=(entry)
 
-    language_of_interest.append(token)
-
-lib_of_interest = []
-for lib in template['technology']:
-    lib = lib.lower()
-    if lib in librarySequence:
-        token = librarySequence[lib] 
-    else:
-        token = librarySequence["none"]    
-        
-    lib_of_interest.append(token)
+    #for intermediate
+    for entry_data in template['intermediate']:
+        entry = utils.gen_classification_data(entry_data, librarySequence, programming_laguage_sequence, classifications_sequence)
+        table+=(entry)
 
 
-header = ["language",
-            "library",
-            "imported_library",
-            "commit_count",
-            "days_since_last_commit",
-            "code_churn",
-            "classificatioin"]
+    my_df = pd.DataFrame(table)
+    my_df.to_csv('truth_template_dataset.csv', index=True)
 
-table = []
-table.append(header)
-
-#for disqualified
-for entry_data in template['disqualified']:
-    entry = utils.gen_classification_data(entry_data, librarySequence, programming_laguage_sequence, classifications_sequence)
-    table+=(entry)
-
-#for expert
-for entry_data in template['expert']:
-    entry = utils.gen_classification_data(entry_data, librarySequence, programming_laguage_sequence, classifications_sequence)
-    table+=(entry)
-
-#for novice
-for entry_data in template['novice']:
-    entry = utils.gen_classification_data(entry_data, librarySequence, programming_laguage_sequence, classifications_sequence)
-    table+=(entry)
-
-#for intermediate
-for entry_data in template['intermediate']:
-    entry = utils.gen_classification_data(entry_data, librarySequence, programming_laguage_sequence, classifications_sequence)
-    table+=(entry)
-
-
-my_df = pd.DataFrame(table)
-my_df.to_csv('my_csv.csv', index=False, header=False)
-
+    return 0
