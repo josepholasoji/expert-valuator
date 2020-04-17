bbef274b2238d00395d6f7bee3b49487fd9f8819
olasoji.joseph <olasoji.joseph@interswitchgroup.com>
2020-04-17 01:41:46 +0100

    Update: code refactoring

--- expert_recommender/project_work_graphql_tensorflow.py
+++ expert_recommender/project_work_graphql_tensorflow.py
@@ -1,61 +1,17 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 7,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import json\n",
-    "import queries as q\n",
-    "import functions as f\n",
-    "\n",
-    "      \n",
-    "#Query the developer details...\n",
-    "developer_details = f.run_query(q.developer_query)\n",
-    "\n",
-    "#print out the developer details for verificatoin purpose on...\n",
-    "# json_formatted_str = json.dumps(own_projects, indent=2)\n",
-    "# print(\"developers projects: {}\".format(json_formatted_str))\n",
-    "\n",
-    "#process the developer's project details\n",
-    "\n",
-    "#for each project detect the language and framework used\n",
-    "#get each framework project's package dependency file and \n",
-    "#process the content to detect the libraries used...\n",
-    "\n",
-    "#\n",
-    "\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "celltoolbar": "Slideshow",
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.6.6"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import json
+import queries as q
+import functions as f
+
+      
+#Query the developer details...
+developer_details = f.run_query(q.developer_query)
+
+#print out the developer details for verificatoin purpose on...
+# json_formatted_str = json.dumps(own_projects, indent=2)
+# print(\"developers projects: {}\".format(json_formatted_str))
+
+#process the developer's project details
+
+#for each project detect the language and framework used
+#get each framework project's package dependency file and 
+#process the content to detect the libraries used...
